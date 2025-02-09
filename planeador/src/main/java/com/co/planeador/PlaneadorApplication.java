d68468f8e14f7df2392dcd4e71150b75e0732b66
carlosfernandoufps <carlosfernandocr@ufps.edu.co>
2025-02-09 12:07:42 -0500

    project structure

---
+++ planeador/src/main/java/com/co/planeador/PlaneadorApplication.java
@@ -0,0 +1,13 @@
+package com.co.planeador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaneadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaneadorApplication.class, args);
+	}
+
+}
