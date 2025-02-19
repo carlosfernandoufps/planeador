5d93a1a4877423f7efe526413082daa4c16d8271
carlosfernandoufps <carlosfernandocr@ufps.edu.co>
2025-02-19 15:27:35 -0500

    fixed institutional-email dto

---
+++ planeador/src/main/java/com/co/planeador/repository/dto/ProfileUserDto.java
@@ -0,0 +1,11 @@
+package com.co.planeador.repository.dto;
+
+public interface ProfileUserDto {
+
+    Integer getId();
+    String getInstitutionalEmail();
+    String getName();
+    String getPersonalEmail();
+    String getPhone();
+    String getCode();
+}
--- planeador/src/main/java/com/co/planeador/service/mapper/ProfileMapper.java
+++ planeador/src/main/java/com/co/planeador/service/mapper/ProfileMapper.java
@@ -1,6 +1,7 @@
 package com.co.planeador.service.mapper;
 
 import com.co.planeador.controller.dto.response.ProfileResponseDto;
+import com.co.planeador.repository.dto.ProfileUserDto;
 import com.co.planeador.repository.entities.Profile;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +11,25 @@ import java.util.List;
 @Component
 public class ProfileMapper {
 
+    public ProfileResponseDto profileToDto(ProfileUserDto profile){
+        ProfileResponseDto dto = new ProfileResponseDto();
+        dto.setCode(profile.getCode());
+        dto.setName(profile.getName());
+        dto.setPhone(profile.getPhone());
+        dto.setPersonalEmail(profile.getPersonalEmail());
+        dto.setId(profile.getId());
+        dto.setInstitutionalEmail(profile.getInstitutionalEmail());
+        return dto;
+    }
+
+    public List<ProfileResponseDto> profileToDto(List<ProfileUserDto> profiles){
+        List<ProfileResponseDto> dtoList = new ArrayList<>();
+        for(ProfileUserDto profile: profiles){
+            dtoList.add(profileToDto(profile));
+        }
+        return dtoList;
+    }
+
     public ProfileResponseDto profileToDto(Profile profile){
         ProfileResponseDto dto = new ProfileResponseDto();
         dto.setCode(profile.getCode());
@@ -18,14 +38,6 @@ public class ProfileMapper {
         dto.setPersonalEmail(profile.getPersonalEmail());
         dto.setId(profile.getId());
         return dto;
-    }
-
-    public List<ProfileResponseDto> profileToDto(List<Profile> profiles){
-        List<ProfileResponseDto> dtoList = new ArrayList<>();
-        for(Profile profile: profiles){
-            dtoList.add(profileToDto(profile));
-        }
-        return dtoList;
     }
 
 
